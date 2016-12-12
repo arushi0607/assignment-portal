090320a1031039a6dd6821a7ad2ff6f4fb548e20
arushi0607 <arushigargdelhi@gmail.com>
2016-12-12 23:04:33 +0530

    new changes

--- db/schema.rb
+++ db/schema.rb
@@ -39,5 +39,4 @@ ActiveRecord::Schema.define(version: 20161211084621) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-
 end
